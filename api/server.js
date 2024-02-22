eeb8219b6e634a530010c04bf5d005a0022690f6
Mohanasundaram B <110714195+Mohansundar25@users.noreply.github.com>
2024-02-22 20:41:57 +0530

    new
     modified:   client/src/App.js

--- api/server.js
+++ api/server.js
@@ -17,8 +17,7 @@ const Todo = require('./models/Todo');
 
 app.get('/todos', async (req, res) => {
 	const todos = await Todo.find();
-
-	res.json(todos);
+    res.json(todos);
 });
 
 app.post('/todo/new', (req, res) => {
